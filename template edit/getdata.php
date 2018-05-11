44c3990307993149b596a24103668198d4061570
safiravanillia <32903307+safiravanillia@users.noreply.github.com>
2018-05-12 06:18:59 +0700

    udah css nya

--- template edit/getdata.php
+++ template edit/getdata.php
@@ -25,36 +25,73 @@
   </head>
 
   <body id="page-top">
-  <div class = "col-md-8 m-4">
-	  <?php
-	 		if($_SESSION["role"] == "freelancer"){
+  	<header>
+  	<!-- Navbar -->
+      <nav class="navbar navbar-expand-lg navbar-light" id="mainNav" data-toggle="affix">
+        <div class="container">
+          <a class="navbar-brand smooth-scroll" href="#">
+            <img src="img/logo-s.png" alt="logo">
+          </a> 
+          <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"> 
+                <span class="navbar-toggler-icon"></span>
+          </button> 
+      </nav>
+    </header> 
+
+<!--====================================================
+                       HOME-P
+======================================================-->
+    <div id="home-p" class="home-p pages-head4 text-center">
+      <div class="container">
+        <h1 class="wow fadeInUp" data-wow-delay="0.1s">SATU LANGKAH LAGI</h1>
+      </div><!--/end container-->
+    </div> 
+
+<!--====================================================
+                        ISI DATA
+======================================================--> 
+    <section id="contact-p1" class="contact-p1">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8">
+            <div class="contact-p1-cont">
+              <h3><br>LENGKAPI DATA DIRIMU</h3>
+              <div class="heading-border-light"></div> 
+            </div>
+          </div> 
+        </div>
+      </div>
+    </section>
+
+  <div class="container">
+  	<div class="row">
+  		<div class="col-md-6">
+  			<?php
+  			if($_SESSION["role"] == "freelancer"){
 	 			echo ' 
-	 				<br />
-					<h3 align="center">Isi data dirimu!</h3>
-					<br />
 					<form method="post" action="insert.php?role=freelancer">
 					<div class = "form-group">
 						Nama Terang
-						<input type="text" name="nama" class ="form-control" placeholder="Nama" required/>
+						<input type="text" name="nama" class ="form-control" placeholder="Masukkan nama" required/>
 					</div>
 					<div class = "form-group">
 						Usia
-						<input type="text" name="umur" class ="form-control" placeholder="usia" required/>
+						<input type="text" name="umur" class ="form-control" placeholder="Masukkan usia" required/>
 					</div>
 					<div class = "form-group">
 						No. Telepon
-						<input type="text" name="telp" class ="form-control" placeholder="No. Telpon" />
+						<input type="text" name="telp" class ="form-control" placeholder="Masukkan nomor telepon" />
 					</div>
 					<div class = "form-group">
 						Keahlian
-						<input type="text" name="ahli" class ="form-control" placeholder="Your title" />
+						<input type="text" name="ahli" class ="form-control" placeholder="Masukkan gelar" />
 					</div>
 					<div class = "form-group">
-						Biodata! <br />
-						<textarea name="bio" rows="10" cols="100"></textarea>
+						Deskripsi <br />
+						<textarea name="bio" rows="10" cols="70" placeholder="Paparkan pengalaman Anda"></textarea>
 					</div>
-					<div class = "form-group">
-						<input type="submit" class="btn btn-primary" value="Submit Data">
+					<div class = "modal-footer">
+						<input type="submit" class="btn btn-general btn-white" value="Simpan">
 					</div>
 					</form>
 						';
@@ -67,23 +104,25 @@
 					<form method="post" action="insert.php?role=pengusaha">
 					<div class = "form-group">
 						Nama Perusahaan
-						<input type="text" name="nama" class ="form-control" placeholder="Nama" required/>
+						<input type="text" name="nama" class ="form-control" placeholder="Masukkan nama" required/>
 					</div>
 					<div class = "form-group">
 						Alamat
-						<input type="text" name="alamat" class ="form-control" placeholder="Alamat Lengkap" required/>
+						<input type="text" name="alamat" class ="form-control" placeholder="Masukkan alamat lengkap" required/>
 					</div>
 					<div class = "form-group">
 						No. Telepon
-						<input type="text" name="telp" class ="form-control" placeholder="No. Telpon" />
+						<input type="text" name="telp" class ="form-control" placeholder="Masukkan nomor telepon" />
 					</div>
 					<div class = "form-group">
-						<input type="submit" class="btn btn-primary" value="Submit Data">
+						<input type="submit" class="btn btn-primary" value="Simpan">
 					</div>
 					</form>
 	 			';
 	 		}
-	  ?>
+	 		?>
+	 		</div>
+	 	</div>
 	</div>
   </body>
  </html>
