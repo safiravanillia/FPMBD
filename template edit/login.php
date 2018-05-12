bd7fc417060906d3ef4f9f0472d80573b6718f76
safiravanillia <32903307+safiravanillia@users.noreply.github.com>
2018-05-12 14:47:46 +0700

    udah bisa cookie remember

--- template edit/login.php
+++ template edit/login.php
@@ -10,31 +10,33 @@
 		die("Connection failed: ". $conn->connect_error);
 	}
 	if($_SERVER["REQUEST_METHOD"] == "POST") {
-		
 		$pass = $_POST['passwd'];
 		$user = $_POST["user"];
-
 		$sql = "SELECT * FROM user WHERE username = '".$user."'";
-		//$query = mysqli_query($conn, $sql);
 		$rs = mysqli_query($conn, $sql);
-
 		while ($row = mysqli_fetch_array($rs)){
 			if(password_verify($pass, $row["password"])){
 				$_SESSION["name"] = $user;
 				$_SESSION["logged"] = true;
-				header('Location:index.php');
 			}
-				else{
-				print '<center>Proses Login GAGAL</center>';
-			}
-		} 
 
-		/*if($rs){
-			$_SESSION["name"] = $user;
-			$_SESSION["logged"] = true;
+			if(!empty($_POST["remember"]) && $_SESSION["logged"] == true)   
+			{  
+				setcookie ("member_login",$user,time()+ (10 * 365 * 24 * 60 * 60));setcookie ("member_password",$pass,time()+ (10 * 365 * 24 * 60 * 60));
+				$_SESSION["admin_name"] = $name;
+			}  elseif (empty($_POST["remember"]) && $_SESSION["logged"] == true)   {  
+				if(isset($_COOKIE["member_login"]))   
+				{  
+					setcookie ("member_login","");  
+				}  
+				if(isset($_COOKIE["member_password"]))   
+				{  
+					setcookie ("member_password","");  
+				}  
+			}
 			header('Location:index.php');
-		}else {
-		print '<center>Proses Login GAGAL</center>';
-		}*/
+		}
+		$_SESSION["gagal"] = true; 
+		header('Location:index.php');
 	}
  ?>
