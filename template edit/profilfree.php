76b765c73871f5687e3e6e2636059faa0f675810
safiravanillia <32903307+safiravanillia@users.noreply.github.com>
2018-05-17 07:22:35 +0700

    +echo deskripsi tp belum nyambung sm bar button

--- template edit/profilfree.php
+++ template edit/profilfree.php
@@ -54,99 +54,144 @@ $sql = "SELECT * FROM freelancer where id = '".$free1['id']."' ";
 
     <style>
     .bio{
-    	width : 300px;
-    	height : 450px;
-    	background-color : #a34e4e;
-    	box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-    	margin-left : 130px;
-    	margin-top : 40px;
-    	float : left;
-    	color : white;
+        width : 300px;
+        height : 450px;
+        background-color : #a34e4e;
+        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+        margin-left : 130px;
+        margin-top : 40px;
+        float : left;
+        color : white;
     }
 
     .details{
-    	width : 700px;
-    	height : 450px;
-    	margin-left : 90px;
-    	margin-top : 40px;
-    	box-shadow: 0px 4px 8px 0 rgba(0, 0, 0, 0.2);
-    	float : left;
+        width : 700px;
+        height : 450px;
+        margin-left : 90px;
+        margin-top : 40px;
+        box-shadow: 0px 4px 8px 0 rgba(0, 0, 0, 0.2);
+        float : left;
     }
 
     .photo{
-    	width : 150px;
-    	height : 150px;
-    	background-color : white;
-    	margin-left : 70px;
-    	margin-top : 50px;
-    	border-radius : 50%;
+        width : 150px;
+        height : 150px;
+        background-color : white;
+        margin-left : 70px;
+        margin-top : 50px;
+        border-radius : 50%;
     }
 
     .bar{
-    	height : 70px;
+        height : 70px;
     }
 
     .bar-button{
-    	width : 200px;
-    	height : 100%;
-    	background-color : white;
-    	float : right;
-    	color : #808282;
-    	margin : center;
-    	font-weight : bold;
-    	text-align : center;
-    	padding-top :25px;
-    	-webkit-transition-duration: 0.4s; /* Safari */
-    	transition-duration: 0.4s;
+        width : 200px;
+        height : 100%;
+        background-color : white;
+        float : right;
+        color : #808282;
+        margin : center;
+        font-weight : bold;
+        text-align : center;
+        padding-top :25px;
+        -webkit-transition-duration: 0.4s; /* Safari */
+        transition-duration: 0.4s;
     }
 
     .bio p{
-    	color : white;
+        color : white;
     }
 
     .bar-button:hover{
-    	background-color : #f0f0f0;
-    	color : #e30066;
-    	border-bottom : 1px solid #e30066;
+        background-color : #f0f0f0;
+        color : #e30066;
+        border-bottom : 1px solid #e30066;
     }
 
     .desc{
-    	width : 650px;
-    	height : 350px;
-    	margin-top : 15px;
-    	margin-left : 25px;
+        width : 650px;
+        height : 350px;
+        margin-top : 15px;
+        margin-left : 25px;
     }
 
     .edit{
-    	width : 120px;
-    	height : 50px;
-    	background-color : #e08a8a;
-    	margin-top : 10px;
-    	margin-left : 130px;
-    	text-align: center;
-   	 	text-decoration: none;
-    	border: none;
-    	outline: none;
-    	cursor: pointer;
-    	-webkit-transition-duration: 0.4s; /* Safari */
-    	transition-duration: 0.4s;
-    	color : white;
+        width : 120px;
+        height : 50px;
+        background-color : #e08a8a;
+        margin-top : 10px;
+        margin-left : 130px;
+        text-align: center;
+        text-decoration: none;
+        border: none;
+        outline: none;
+        cursor: pointer;
+        -webkit-transition-duration: 0.4s; /* Safari */
+        transition-duration: 0.4s;
+        color : white;
     }
 
     .edit:hover{
-    	background-color : #c57575;
+        background-color : #c57575;
     }
 
     .edit a{
-    	text-decoration: none;
-    	color: white;
-    	display: block;
+        text-decoration: none;
+        color: white;
+        display: block;
     }
     </style>
    </head>
 
 <body id = "page-top">
-<nav class="navbar navbar-expand-lg navbar-light" id="mainNav" data-toggle="affix">
+<!--====================================================
+                         HEADER
+======================================================--> 
+
+    <header>
+
+      <!-- Top Navbar  -->
+      <div class="top-menubar">
+        <div class="topmenu">
+          <div class="container">
+            <div class="row">
+              <div class="col-md-7">
+                <ul class="list-inline top-contacts">
+                  <li>
+                    <i class="fa fa-envelope"></i> <a href="mailto:imail@freelancemosv.id" class="">mail@freelancemosv.id</a>
+                  </li>
+                  <li>
+                    <i class="fa fa-phone"></i> (031) 3982200
+                  </li>
+                </ul>
+              </div> 
+              <div class="col-md-5">
+                <ul class="list-inline top-data"> 
+                <?php
+                  if(isset($_SESSION["logged"])){
+                    echo '
+                    <li><div class = "log-top">Selamat datang, '.$_SESSION["name"].'!</div></li>
+                    <li><a href="logout.php" class="log-top"">Keluar</a></li>';
+                  }elseif (isset($_SESSION["gagal"])) {
+                    echo '
+                    <li><a href="logout.php" class="log-top"> login gagal, coba lagi </a></li>';
+                  }
+                  else{
+                    echo '
+                    <li><a href="#" class="log-top" data-toggle="modal" data-target="#login-modal">Masuk</a></li>';
+                  }
+                ?>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div> 
+      </div> 
+      
+      <!-- Navbar -->
+      <nav class="navbar navbar-expand-lg navbar-light" id="mainNav" data-toggle="affix">
         <div class="container">
           <a class="navbar-brand smooth-scroll" href="index.php">
             <img src="img/logo-s.png" alt="logo">
@@ -178,7 +223,7 @@ $sql = "SELECT * FROM freelancer where id = '".$free1['id']."' ";
                   <i class="search fa fa-search search-btn"></i>
                   <div class="search-open">
                     <div class="input-group animated fadeInUp">
-                      <input type="text" class="form-control" placeholder="Ketikkan Kategori" aria-describedby="basic-addon2">
+                      <input type="text" class="form-control" placeholder="Ketikkan Pekerjaan" aria-describedby="basic-addon2">
                       <span class="input-group-addon" id="basic-addon2">Cari</span>
                     </div>
                   </div>
@@ -212,27 +257,101 @@ $sql = "SELECT * FROM freelancer where id = '".$free1['id']."' ";
           </div>
         </div>
       </nav>
-    <br>
+    </header> 
+
+<!--====================================================
+                    LOGIN OR REGISTER
+======================================================-->
+    <section id="login">
+      <div class="modal fade" id="login-modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" style="display: none;">
+          <div class="modal-dialog">
+              <div class="modal-content">
+                  <div class="modal-header" align="center">
+                      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                          <span class="fa fa-times" aria-hidden="true"></span>
+                      </button>
+                  </div>
+                  <div id="div-forms">
+                      <form id="login-form" method="post" action="login.php">
+                          <h3 class="text-center">Masuk</h3>
+                          <div class="modal-body">
+                              <label for="username">Username</label> 
+                              <input id="login_username" class="form-control" type="text" placeholder="Masukkan username " name="user" value="<?php if(isset($_COOKIE["member_login"])) { echo $_COOKIE["member_login"]; } ?>" required>
+                              <label for="username">Kata Sandi</label> 
+                              <input id="login_password" class="form-control" type="password" placeholder="Masukkan kata sandi" name="passwd" value="<?php if(isset($_COOKIE["member_password"])) { echo $_COOKIE["member_password"]; } ?>"required>
+                              <div class="checkbox">
+                                  <label>
+                                      <input type="checkbox" name="remember"<?php if(isset($_COOKIE["member_login"])) { ?> checked <?php } ?> > Ingat saya!
+                                  </label>
+                              </div>
+                          </div>
+                          <div class="modal-footer text-center">
+                              <div>
+                                  <button type="submit" class="btn btn-general btn-white">Masuk</button>
+                              </div>
+                              <div>
+                                  <button id="login_register_btn" type="button" class="btn btn-link">Daftar</button>
+                              </div>
+                          </div>
+                      </form>
+                      <form id="register-form" style="display:none;" method="post" action="register.php">
+                          <h3 class="text-center">Daftar</h3>
+                          <div class="modal-body"> 
+                              <label for="username">Username</label> 
+                              <input id="register_username" class="form-control" type="text" placeholder="Masukkan username" name="user" required>
+                              <label for="register_email">E-mail</label> 
+                              <input id="register_email" name="email" class="form-control" type="text" placeholder="Masukkan E-mail" required>
+                              <label for="register_password">Kata Sandi</label> 
+                              <input id="register_password" name="passwd" class="form-control" type="password" placeholder="Masukkan kata sandi" required>
+                              <label for="register_password">Pilih peran:</label>
+                              <div class="checkbox"> 
+                                <label>
+                                  <input type="radio" name ="role" value="freelancer" checked="checked"> Freelancer
+                                </label>
+                              </div>
+                              <div class="checkbox">
+                                <label>
+                                  <input type="radio" name="role" value="pengusaha"> Pengusaha
+                                </label>
+                              </div>
+                          <div class="modal-footer">
+                              <div>
+                                  <button type="submit" class="btn btn-general btn-white">Daftar</button>
+                              </div>
+                              <div>
+                                  <button id="register_login_btn" type="button" class="btn btn-link">Masuk</button>
+                              </div>
+                          </div>
+                      </form>
+                  </div>
+              </div>
+          </div>
+      </div>
+    </section>      
+
 
    
 <div class = "bio">
-	<div class ="photo">
-	</div>
-	<p style = "margin-top : 20px; margin-left : 20px;">Nama : <?php echo $name;?></p> 
-	<p style = "margin-left : 20px;">Usia : <?php echo $usia;?></p>
-	<p style = "margin-left : 20px">Keahlian : <?php echo $master;?></p>
-	<p style = "margin-left : 20px">No. Telp : <?php echo $telpon;?></p>
-	<p style = "margin-left : 20px">E-mail : <?php echo $email;?></p>
+    <div class ="photo">
+    </div>
+    <p style = "margin-top : 20px; margin-left : 20px;">Nama : <?php echo $name;?></p> 
+    <p style = "margin-left : 20px;">Usia : <?php echo $usia;?></p>
+    <p style = "margin-left : 20px">Keahlian : <?php echo $master;?></p>
+    <p style = "margin-left : 20px">No. Telp : <?php echo $telpon;?></p>
+    <p style = "margin-left : 20px">E-mail : <?php echo $email;?></p>
 </div>
 <div class = "details">
-	<div class = "bar">
-		<div class = "bar-button"><b>Review</b></div>
-		<div class = "bar-button">Portofolio</div>
-		<div class = "bar-button">Histori Pekerjaan</div>
-	</div>
-	<div class = "desc">
-		ini isinya
-	</div>
+    <div class = "bar">
+        <div class = "bar-button"><b>Review</b></div>
+        <div class = "bar-button">Portofolio</div>
+        <div class = "bar-button">Histori Pekerjaan</div>
+    </div>
+    <div class = "desc">
+        <?php echo $description;?>
+    </div>
+    <div class = "desc">
+        <img src="images/<?php echo $portofolio; ?>">
+    </div>
 </div>
 <br />
 <button class = "edit"><?php echo '<a href ="form-edit1.php?id='.$id.'">';?>Ubah Profil</a></button>
