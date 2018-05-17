4868d32fcc8602cc8cc0b0d1f5fcca383cf1a7c5
safiravanillia <32903307+safiravanillia@users.noreply.github.com>
2018-05-17 07:03:03 +0700

    tambahan form portofolio tp masih gagal uploadnya

--- template edit/register.php
+++ template edit/register.php
@@ -3,36 +3,33 @@
 	$servername = "localhost";
 	$username = "root";
 	$password = "";
-	$dbname = "mosv";
+	$dbname = "pweb";
 	
 	$conn = new mysqli($servername, $username, $password, $dbname);
 	if($conn->connect_error){
 		die("Connection failed: ". $conn->connect_error);
 	}
-
 	if($_SERVER["REQUEST_METHOD"]=="POST"){
-		
-		$options = [
-			'cost' => 11,
-		];
-
 		$pass = $_POST['passwd'];
-		$hash = password_hash($pass, PASSWORD_BCRYPT, $options);
-
 		$user = $_POST["user"];
 		$email = $_POST["email"];
-		$role = $_POST["role"];
-
-		$sql = "
-			INSERT INTO user VALUES(NULL, '$user', '$email', '$hash', '$role');
-		";
+		$nrp = $_POST["nrp"];
+		$depan = $_POST["depan"];
+		$blkg = $_POST["blkg"];
+		$ttl = $_POST["ttl"];
+		$kota = $_POST["kota"];
+		$asal = $_POST["asal"];
+		$sby = $_POST["sby"];
+		$telp = $_POST["telp"];
+		$agama = $_POST["agama"];
+		$foto = $_FILES['foto']['name'];
+		$sql = "INSERT INTO user VALUES(NULL, '$user', '$email', '$pass', '$nrp', '$depan', '$blkg', '$ttl', '$kota', '$asal', '$sby', '$telp', '$agama', '$foto');";
+		move_uploaded_file($_FILES['foto']['tmp_name'],'images/'.$foto);
 		$query = mysqli_query($conn, $sql);
-
 		if($query){
 			$_SESSION["logged"] = true;
 			$_SESSION["name"] = $user;
-			$_SESSION["role"] = $role;
-			header("location:getdata.php");	
+			header("location:index.php");	
 		}
 	}
 ?>
