2b60fd38cdfa83197b6bc36b798b066b8e929405
safiravanillia <32903307+safiravanillia@users.noreply.github.com>
2018-05-18 23:17:57 +0700

    tambahan kemungkinan if

--- template edit/prosesedit1.php
+++ template edit/prosesedit1.php
@@ -11,23 +11,42 @@ session_start();
   if($conn->connect_error){
     die("Connection failed: ". $conn->connect_error);
   }
-// cek apakah tombol simpan sudah diklik atau blum?
+
 if(isset($_POST['simpan'])){
 
-    // ambil data dari formulir
     $id = $_POST['id'];
     $nama = $_POST['nama'];
     $usia = $_POST['umur'];
     $telepon = $_POST['telp'];
     $ahli = $_POST['ahli'];
     $biodata = $_POST['bio'];
+    $porto = $_FILES["porto"]["name"];
+    $foto = $_FILES["foto"]["name"];
 
-    // buat query update
-    $sql = "UPDATE freelancer SET f_nama='$nama', f_usia=$usia, f_telepon=$telepon, f_ahli='$ahli', f_deskripsi='$biodata' WHERE id=$id";
-    echo $sql;
-    $query = mysqli_query($conn, $sql);
+    if(isset($_FILES["porto"]) && isset($_FILES["foto"])){
+      if(!empty($porto) && !empty($foto)){      
+        move_uploaded_file($_FILES['porto']['tmp_name'],'images/'.$porto);
+        move_uploaded_file($_FILES['foto']['tmp_name'],'foto/'.$foto);
+        $sql = "UPDATE freelancer SET f_nama='$nama', f_usia=$usia, f_telepon=$telepon, f_ahli='$ahli', f_deskripsi='$biodata', f_portofolio='$porto', picture='$foto' WHERE id='$id'";
+        echo $sql;
+        $query = mysqli_query($conn, $sql);
+      }elseif(empty($porto) && !empty($foto)){
+        move_uploaded_file($_FILES['foto']['tmp_name'],'foto/'.$foto);
+        $sql = "UPDATE freelancer SET f_nama='$nama', f_usia=$usia, f_telepon=$telepon, f_ahli='$ahli', f_deskripsi='$biodata', picture='$foto' WHERE id='$id'";
+        echo $sql;
+        $query = mysqli_query($conn, $sql);
+      }elseif(!empty($porto) && empty($foto)){
+        move_uploaded_file($_FILES['porto']['tmp_name'],'images/'.$porto);
+        $sql = "UPDATE freelancer SET f_nama='$nama', f_usia=$usia, f_telepon=$telepon, f_ahli='$ahli', f_deskripsi='$biodata', f_portofolio='$porto' WHERE id='$id'";
+        echo $sql;
+        $query = mysqli_query($conn, $sql);
+      }else{
+        $sql = "UPDATE freelancer SET f_nama='$nama', f_usia=$usia, f_telepon=$telepon, f_ahli='$ahli', f_deskripsi='$biodata' WHERE id=$'id'";
+        echo $sql;
+        $query = mysqli_query($conn, $sql);
+      }
+    }
 
-    // apakah query update berhasil?
     if($query) {
         header('Location: profilfree.php');
     } else {
