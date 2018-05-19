c7daca9754465099dd7afc272d7d0b98c0f8ddc2
safiravanillia <32903307+safiravanillia@users.noreply.github.com>
2018-05-19 07:15:58 +0700

    insert sesuai new db+ fix bug navbar abis daftar

--- template edit/insert.php
+++ template edit/insert.php
@@ -25,15 +25,27 @@
 		$telp = $_POST["telp"];
 		$ahli = $_POST["ahli"];
 		$bio = $_POST["bio"];
-		$hasil = $_FILES['foto']['name'];
+		$hasil = $_FILES['porto']['name'];
 
-		move_uploaded_file($_FILES['foto']['tmp_name'],'images/'.$hasil);
+		move_uploaded_file($_FILES['porto']['tmp_name'],'images/'.$hasil);
 		$s = "INSERT INTO freelancer(id,f_nama,f_usia,f_telepon,f_ahli,f_deskripsi,f_portofolio) values ('$id', '$nama', '$umur', '$telp', '$ahli', '$bio', '$hasil');";
 
-		$_SESSION["role"] = "freelancer";
-		$_SESSION["name"] = $user;
-		$_SESSION["logged"] = true;
-		$_SESSION["free"] = true;
+		$sql = "SELECT * FROM user WHERE id = '".$id."'";
+
+		$rs = mysqli_query($conn, $sql);
+		while ($row = mysqli_fetch_array($rs)){
+			if($rs&&$row["role"]=="freelancer"){
+				$_SESSION["role"] = "freelancer";
+				$_SESSION["name"] = $row["username"];
+				$_SESSION["logged"] = true;
+				$_SESSION["free"] = true;
+			}
+		}
+		$q = mysqli_query($conn, $s);
+		if($q){
+		mysqli_close();
+		header("location:index.php");
+		}
 	}
 	else if($_GET["role"] == "pengusaha"){
 		$nama = $_POST["nama"];
@@ -41,15 +53,22 @@
 		$telp = $_POST["telp"];
 
 		$s = "INSERT INTO pengusaha(pengusaha_id,nama,alamat,telepon) values ('$id', '$nama', '$alamat', '$telp');";
-		$_SESSION["role"] = "pengusaha";
-				$_SESSION["name"] = $user;
+
+		$sql = "SELECT * FROM user WHERE id = '".$pengusaha_id."'";
+
+		$rs = mysqli_query($conn, $sql);
+		while ($row = mysqli_fetch_array($rs)){
+			if($rs&&$row["role"]=="pengusaha"){
+				$_SESSION["role"] = "pengusaha";
+				$_SESSION["name"] = $row["username"];
 				$_SESSION["logged"] = true;
 				$_SESSION["peng"] = false;
-	}
-
-	$q = mysqli_query($conn, $s);
-	if($q){
+			}
+		}
+		$q = mysqli_query($conn, $s);
+		if($q){
 		mysqli_close();
 		header("location:index.php");
+		}
 	}
 ?>
