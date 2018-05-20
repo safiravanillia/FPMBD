8e275cdb1883136fd32c07a2a4d184b7f6bd7129
safiravanillia <32903307+safiravanillia@users.noreply.github.com>
2018-05-20 15:28:38 +0700

    dah bisa ke update finally :"

--- template edit/prosesedit2.php
+++ template edit/prosesedit2.php
@@ -22,16 +22,16 @@ if(isset($_POST['simpan'])){
     $foto = $_FILES["foto"]["name"];    
 
     if(isset($_FILES["foto"])){
-    	$foto_tmp = addslashes(file_get_contents($_FILES["foto"]["tmp_name"]));
-		$foto_type = addslashes($_FILES["foto"]["type"]);
+      $foto_tmp = addslashes(file_get_contents($_FILES["foto"]["tmp_name"]));
+      $foto_type = addslashes($_FILES["foto"]["type"]);
       if(!empty($foto)){      
         /*move_uploaded_file($_FILES['foto']['tmp_name'],'foto/'.$foto);*/
         $sql = "UPDATE pengusaha SET nama = '$nama', alamat = '$alamat', telepon = '$telepon' , deskripsi = '$deskripsi', picture='$foto_tmp' WHERE pengusaha_id = '$id'";
-        echo $sql;
+        //echo $sql;
         $query = mysqli_query($conn, $sql);
       }else{
         $sql = "UPDATE pengusaha SET nama = '$nama', alamat = '$alamat', telepon = '$telepon' , deskripsi = '$deskripsi' WHERE pengusaha_id = '$id'";
-        echo $sql;
+        //echo $sql;
         $query = mysqli_query($conn, $sql);
       }
     }
